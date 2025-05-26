6be9335c007026b2789e2bfb4cd0e3500cdd3506
AltantL <antoine.lapluye@gmail.com>
2025-05-26 16:06:43 +0200

    upgrade GridPaneSelectionAlgo

--- src/main/java/vue/GridPaneSelectionAlgo.java
+++ src/main/java/vue/GridPaneSelectionAlgo.java
@@ -1,4 +1,34 @@
 package vue;
 
-public class GridPaneSelectionAlgo {
+import constantes.ConstanteIHM;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+
+public class GridPaneSelectionAlgo extends GridPane implements ConstanteIHM {
+    public GridPaneSelectionAlgo() {
+
+        Label labelTitre = new Label("SAE JAVA IHM");
+
+        ToggleGroup radioGroup = new ToggleGroup();
+
+        RadioButton radioButton1 = new RadioButton("Algo 1");
+        RadioButton radioButton2 = new RadioButton("Algo 2");
+        RadioButton radioButton3 = new RadioButton("Algo 3");
+
+        radioGroup.getToggles().addAll(radioButton1, radioButton2, radioButton3);
+
+        ComboBox comboScenarios = new ComboBox();
+        comboScenarios.getItems().addAll(SCENARIOS);
+
+        this.add(labelTitre, 0, 0);
+
+        this.add(radioButton1, 1, 1);
+        this.add(radioButton2, 1, 2);
+        this.add(radioButton3, 1, 3);
+
+        this.add(comboScenarios, 2, 1);
+
+
+
+    }
 }
