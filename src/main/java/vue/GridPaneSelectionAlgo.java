5587bf921ec981e160017b98aecbf4ea168b5383
AltantL <antoine.lapluye@gmail.com>
2025-05-28 17:26:00 +0200

    Maj GridPaneSelectionAlgo

--- src/main/java/vue/GridPaneSelectionAlgo.java
+++ src/main/java/vue/GridPaneSelectionAlgo.java
@@ -1,10 +1,15 @@
 package vue;
 
 import constantes.ConstanteIHM;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
 public class GridPaneSelectionAlgo extends GridPane implements ConstanteIHM {
+
+    ComboBox comboScenarios;
+
     public GridPaneSelectionAlgo() {
 
         Label labelTitre = new Label("SAE JAVA IHM");
@@ -15,20 +20,44 @@ public class GridPaneSelectionAlgo extends GridPane implements ConstanteIHM {
         RadioButton radioButton2 = new RadioButton("Algo 2");
         RadioButton radioButton3 = new RadioButton("Algo 3");
 
+        radioButton1.setSelected(true);
+
         radioGroup.getToggles().addAll(radioButton1, radioButton2, radioButton3);
 
-        ComboBox comboScenarios = new ComboBox();
-        comboScenarios.getItems().addAll(SCENARIOS);
+        comboScenarios = peupleComboBox(SCENARIOS);
+
+        Button boutonAnnuler = new Button("Annuler");
+
+        boutonAnnuler.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                radioButton1.setSelected(true);
+                comboScenarios.setValue(SCENARIOS[0]);
+            }
+        });
+
+        Button bouttonValider = new Button("Valider");
 
         this.add(labelTitre, 0, 0);
 
-        this.add(radioButton1, 1, 1);
-        this.add(radioButton2, 1, 2);
-        this.add(radioButton3, 1, 3);
+        this.add(radioButton1, 0, 1);
+        this.add(radioButton2, 0, 2);
+        this.add(radioButton3, 0, 3);
 
-        this.add(comboScenarios, 2, 1);
+        this.add(comboScenarios, 1, 1);
+
+        this.add(boutonAnnuler, 0, 5);
+        this.add(bouttonValider, 1, 5);
 
 
+    }
 
+    private ComboBox<String> peupleComboBox(String[] strings) {
+        ComboBox<String> comboBox = new ComboBox<>();
+        for(String string : strings) {
+            comboBox.getItems().add(string);
+        }
+        comboBox.setValue(strings[0]);
+        return comboBox;
     }
 }
