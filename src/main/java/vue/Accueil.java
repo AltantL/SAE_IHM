24ed7bcd32c3dc30df70ea733f6a8eaeab6e63e7
AltantL <antoine.lapluye@gmail.com>
2025-05-28 17:24:55 +0200

    Creation Accueil

---
+++ src/main/java/vue/Accueil.java
@@ -0,0 +1,9 @@
+package vue;
+
+import javafx.scene.layout.GridPane;
+
+public class Accueil extends GridPane {
+    public Accueil() {
+        //ici y faut mettre tous pour faire une page d'accueil
+    }
+}
