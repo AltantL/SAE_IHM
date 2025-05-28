a4eac424af26bd5c332d67f4db57a2309d330572
AltantL <antoine.lapluye@gmail.com>
2025-05-28 17:25:26 +0200

    Creation Resultat

---
+++ src/main/java/vue/Resultat.java
@@ -0,0 +1,14 @@
+package vue;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class Resultat extends VBox {
+    public Resultat() {
+        Label labelTitre = new Label("Resultat");
+
+        this.getChildren().add(labelTitre);
+
+
+    }
+}
