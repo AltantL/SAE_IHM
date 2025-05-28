d20af5f87a2bf998d3e63334c5cc8220850031b1
AltantL <antoine.lapluye@gmail.com>
2025-05-28 17:25:13 +0200

    Creation Controleur

---
+++ src/main/java/controleur/Controleur.java
@@ -0,0 +1,74 @@
+package controleur;
+
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import modele.*;
+import vue.GridPaneSelectionAlgo;
+import vue.HBoxRoot;
+import vue.Resultat;
+
+
+public class Controleur implements EventHandler{
+
+//    PlanningCollection planning;
+
+    @Override
+    public void handle(Event event) {
+        GridPaneSelectionAlgo selectionAlgo = HBoxRoot.getGridPaneSelectionAlgo();
+        Resultat resultat = HBoxRoot.getResultat();
+
+        //la source de event est le bouton "Enregistrer" du formulaire de selection
+        if (event.getSource() instanceof ToggleButton) {
+
+            //
+
+        }
+
+        //la source de event est le bouton "Enregistrer" du formulaire de réservatuion
+
+        if (event.getSource() instanceof Button){
+//
+//            Date date = reservationPane.dateReserve;
+//
+//            TextField textField = reservationPane.textField;
+//
+//
+//            ComboBox heureDepart = reservationPane.heureDepart;
+//            ComboBox minuteDepart = reservationPane.minuteDepart;
+//            ComboBox heureFin = reservationPane.heureFin;
+//            ComboBox minuteFin = reservationPane.minuteFin;
+//
+//            int heureD = heureDepart.getSelectionModel().getSelectedIndex()+6;
+//            int minuteD = minuteDepart.getSelectionModel().getSelectedIndex()*15;
+//            int heureF = heureFin.getSelectionModel().getSelectedIndex()+6;
+//            int minuteF = minuteFin.getSelectionModel().getSelectedIndex()*15;
+//
+//            Horaire horaire1 = new Horaire(heureD,minuteD);
+//            Horaire horaire2 = new Horaire(heureF,minuteF);
+//
+//
+//
+//            PlageHoraire plageHoraire = null;
+//            try {
+//                plageHoraire = new PlageHoraire(horaire1,horaire2);
+//                Reservation reservation = new Reservation(textField.getText(), date, plageHoraire);
+//
+//                HBoxRoot.getPlanning().ajout(reservation);
+//                System.out.println(reservation);
+//                System.out.println(HBoxRoot.getPlanning().toString());
+//
+//            } catch (ExceptionPlanning e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//            affichagePlanning.setNumSemaineSelectionnee(date);
+
+        }
+//        if (event.getSource() instanceof .......){
+//            //
+//        }
+    }
+}
