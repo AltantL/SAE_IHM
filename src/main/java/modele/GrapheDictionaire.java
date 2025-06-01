6472cf15644aa87bc93fa27dc92022d716081092
julienx-hub <julien.272737@gmail.com>
2025-06-01 14:19:50 +0200

    conversion de la steructure vers graphe

--- src/main/java/modele/GrapheDictionaire.java
+++ src/main/java/modele/GrapheDictionaire.java
@@ -1,5 +1,9 @@
 package modele;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.*;
 
 public class GrapheDictionaire {
@@ -12,5 +16,38 @@ public class GrapheDictionaire {
         nomToIndice = new HashMap<>();
         indiceToNom = new HashMap<>();
         int index = 0;
+        for (String ville : villeVendeurPlus.keySet()) {
+            if (!nomToIndice.containsKey(ville)) {
+                nomToIndice.put(ville, index);
+                indiceToNom.put(index, ville);
+                index++;
+            }
+            for (String voisin : villeVendeurPlus.get(ville)) {
+                if (!nomToIndice.containsKey(voisin)) {
+                    nomToIndice.put(voisin, index);
+                    indiceToNom.put(index, voisin);
+                    index++;
+                }
+            }
+        }
+
+        int nbSommets = nomToIndice.size();
+        int[][] tableauVoisins = new int[nbSommets][];
+        for (int i = 0; i < nbSommets; i++) {
+            tableauVoisins[i] = new int[0];
+        }
+        for (String ville : villeVendeurPlus.keySet()) {
+            int sommet = nomToIndice.get(ville);
+            ArrayList<String> voisinsNoms = villeVendeurPlus.get(ville);
+            int[] voisinsIndices = new int[voisinsNoms.size()];
+            for (int i = 0; i < voisinsNoms.size(); i++) {
+                voisinsIndices[i] = nomToIndice.get(voisinsNoms.get(i));
+            }
+            tableauVoisins[sommet] = voisinsIndices;
+        }
+        graphe = new Graphe(tableauVoisins);
     }
+
+
+
 }
