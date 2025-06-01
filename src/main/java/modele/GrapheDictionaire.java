0f6eda1115f7e54e781caa234d5f5122590f6ea9
julienx-hub <julien.272737@gmail.com>
2025-06-01 14:06:23 +0200

    constructeur + struucture de données

--- src/main/java/modele/GrapheDictionaire.java
+++ src/main/java/modele/GrapheDictionaire.java
@@ -1,4 +1,16 @@
 package modele;
 
+import java.util.*;
+
 public class GrapheDictionaire {
+    private TreeMap<Integer, Set<Integer>> mapSommetsVoisins;
+    private Map<String, Integer> nomToIndice;
+    private Map<Integer, String> indiceToNom;
+    private Graphe graphe;
+    public GrapheDictionaire(Map<String, ArrayList<String>> villeVendeurPlus) {
+        mapSommetsVoisins = new TreeMap<>();
+        nomToIndice = new HashMap<>();
+        indiceToNom = new HashMap<>();
+        int index = 0;
+    }
 }
