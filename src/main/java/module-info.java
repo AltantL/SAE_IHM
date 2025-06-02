d437d30aebc2485da51f26aa315300472d141bae
AltantL <antoine.lapluye@gmail.com>
2025-06-02 19:55:54 +0200

    desktop

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sae_2012 {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.desktop;
 
     opens com.example.sae_2012 to javafx.fxml;
     exports com.example.sae_2012;
