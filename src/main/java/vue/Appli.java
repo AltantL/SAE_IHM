397d99d80042124b30cce7c7a3571035910ce80c
AltantL <antoine.lapluye@gmail.com>
2025-06-02 19:54:42 +0200

    simplification de l'Appli

--- src/main/java/vue/Appli.java
+++ src/main/java/vue/Appli.java
@@ -2,6 +2,7 @@ package vue;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -10,10 +11,17 @@ public class Appli extends Application {
 
     public void start(Stage stage) {
 
+
+
         stage.setTitle("APPLI");
         HBoxRoot root = new HBoxRoot();
         Accueil accueil = new Accueil();
-        Scene scene = new Scene(root,690, 338);
+        GridPaneCreationScenario grid = new GridPaneCreationScenario();
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(grid);
+
+        Scene scene = new Scene(accueil,690, 338);
         File [] css = new File("css").listFiles();
         for(File fichier : css){
             scene.getStylesheets().add(fichier.toURI().toString());
