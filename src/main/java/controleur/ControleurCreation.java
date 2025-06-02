ae6fdf3317aa0544425e44e21b6b366805d84272
AltantL <antoine.lapluye@gmail.com>
2025-06-02 19:47:32 +0200

    à suprimer

--- src/main/java/controleur/ControleurCreation.java
+++ src/main/java/controleur/ControleurCreation.java
@@ -18,39 +18,6 @@ public class ControleurCreation {
         this.root = root;
     }
 
-    private void instanciateButtons(){
-        this.instanciationCreerScenario();
-    }
 
-    public final void instanciationCreerScenario() {
-        this.root.getBouttonValider().setOnAction(event -> {
-            try {
-                creationScenario();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-
-    private void creationScenario() throws IOException {
-        String text = this.root.getTextFieldNomScenario().toString();
-
-        ComboBox comboCreationScenarios1 = GridPaneCreationScenario.getComboBoxCreationScenarios1();
-        ComboBox comboCreationScenarios2 = GridPaneCreationScenario.getComboBoxCreationScenarios2();
-
-        String comboCreationScenarios11 = comboCreationScenarios1.getSelectionModel().toString();
-        String comboCreationScenarios22 = comboCreationScenarios2.getSelectionModel().toString();
-
-        String[] test = new String[]{comboCreationScenarios11 + " --> " + comboCreationScenarios22 + "\n"};
-
-        String test2 = test.toString();
-
-        String fileData = test2;
-        FileOutputStream fos = new FileOutputStream("scenarioCreer/" + File.separator + text + ".txt");
-        fos.write(fileData.getBytes());
-        fos.flush();
-        fos.close();
-
-    }
 
 }
