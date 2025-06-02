ca417e5e4d050d43176a93abab13b20ed14d5d3d
AltantL <antoine.lapluye@gmail.com>
2025-06-02 19:51:15 +0200

    Maj Resultat

--- src/main/java/vue/Resultat.java
+++ src/main/java/vue/Resultat.java
@@ -15,21 +15,18 @@ public class Resultat extends VBox {
     public Resultat() {
         Label labelTitre = new Label("Resultat");
 
-        testResultat1 = new ArrayList<>();
-        String testResultat2 = testResultat1.toString();
-        Label testResultat3 = new Label(testResultat2);
-
         this.getChildren().add(labelTitre);
-        this.getChildren().add(testResultat3);
-
 
     }
 
     public void affichageResultat (String senario) throws IOException {
 
         ArrayList<Map> testResultat;
-        testResultat = LectureScenario.regrouperParVille(senario);
+        testResultat = LectureScenario.regrouperParVille(senario+".txt");
         System.out.println(testResultat);
         testResultat1 = testResultat;
+        Label testResultat2 = new Label(testResultat1.toString());
+        this.getChildren().add(testResultat2);
+
     }
 }
