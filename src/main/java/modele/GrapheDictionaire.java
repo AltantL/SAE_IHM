94110d3499935ad86046fa3fe5a330117197a27b
julienx-hub <julien.272737@gmail.com>
2025-06-03 22:36:36 +0200

    update GrapheDictionaire

--- src/main/java/modele/GrapheDictionaire.java
+++ src/main/java/modele/GrapheDictionaire.java
@@ -1,3 +1,21 @@
 package modele;
+
+import java.util.*;
+
 public class GrapheDictionaire {
+    private Map<String, List<String>> mapSommetsVoisins;
+
+    public GrapheDictionaire(Map<String, ArrayList<String>> structure) {
+        mapSommetsVoisins = new HashMap<>();
+        for (Map.Entry<String, ArrayList<String>> entry : structure.entrySet()) {
+            mapSommetsVoisins.put(entry.getKey(), new ArrayList<>(entry.getValue()));
+        }
+        for (List<String> voisins : structure.values()) {
+            for (String voisin : voisins) {
+                mapSommetsVoisins.putIfAbsent(voisin, new ArrayList<>());
+            }
+        }
+    }
+
+
 }
