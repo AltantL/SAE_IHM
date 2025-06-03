4b55e9000dda1f192a9a04203c827504e143b821
julienx-hub <julien.272737@gmail.com>
2025-06-03 23:38:12 +0200

    ClientGrapheDictionaire

--- src/main/java/client/ClientGrapheDictionaire.java
+++ src/main/java/client/ClientGrapheDictionaire.java
@@ -2,6 +2,7 @@ package client;
 
 import modele.GrapheDictionaire;
 import modele.GrapheLivraison;
+import modele.LectureScenario;
 import modele.Scenario;
 
 import java.io.IOException;
@@ -13,9 +14,17 @@ import java.util.Map;
 public class ClientGrapheDictionaire {
     public static void main(String[] args) throws IOException {
         Map<String, ArrayList<String>> donnees = GrapheLivraison.grapheLivraison("scenario_0.txt");
+        HashMap<String, HashMap<String, Integer>> distance = LectureScenario.lectureDistance();
+
+        Integer test = LectureScenario.getDistance(distance,"Amiens","Amiens");
+
+        System.out.println(test);
         GrapheDictionaire graphe = new GrapheDictionaire(donnees);
-        List<String> ordre = graphe.triTopologique();
+        List<String> ordre = graphe.triTopologique();  // contient le tri
+        Integer testDistanceTot = LectureScenario.calculerDistanceTotale(ordre,distance); // contient la distance total du tri
+        System.out.println(testDistanceTot);
         System.out.println(ordre);
+
 
     }
 }
