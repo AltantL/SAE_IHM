7cfc4caddbf0e7148a622f5cf1676c810f4e127f
AltantL <antoine.lapluye@gmail.com>
2025-06-04 04:10:16 +0200

    Fin Resultat

--- src/main/java/vue/Resultat.java
+++ src/main/java/vue/Resultat.java
@@ -2,37 +2,57 @@ package vue;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import modele.GrapheDictionaire;
+import modele.GrapheLivraison;
 import modele.LectureScenario;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Resultat extends VBox {
 
-    private static ArrayList<Map> testResultat1;
-
     public Resultat() {
         Label labelTitre = new Label("Resultat");
-
         this.getChildren().add(labelTitre);
+    }
 
+    public void nettoyage(){
+        this.getChildren().clear();
     }
 
     public void affichageResultat (String senario) throws IOException {
 
-        ArrayList<Map> testResultat;
-        testResultat = LectureScenario.regrouperParVille(senario+".txt");
-        System.out.println(testResultat);
-        testResultat1 = testResultat;
+        String algo = HBoxSelectionAlgo.getComboAlgo();
 
-        for (Map i : testResultat) {
-            Label testResultat2 = new Label(i.toString());
-            this.getChildren().add(testResultat2);
+        Map<String, ArrayList<String>> donnees = GrapheLivraison.grapheLivraison(senario);
+        GrapheDictionaire graphe = new GrapheDictionaire(donnees);
+        List<String> testResultat = null;
+        HashMap<String, HashMap<String, Integer>> distance = LectureScenario.lectureDistance();
+
+        if (algo.equals("triTopologique")){
+            testResultat = graphe.triTopologique();
+        }
+        if (algo.equals("heuristique")){
+            testResultat = graphe.heuristique();
+        }
+        if (algo.equals("triAStar")){
+            testResultat = graphe.triAStar(distance);
         }
 
+        List<String> ordre = testResultat;
 
+        Integer testDistance = LectureScenario.calculerDistanceTotale(ordre,distance);
 
+        Label labelalgo = new Label("Algorithme utiliser : " + algo);
+        Label labelDistance = new Label("Distance : " + testDistance);
+        Label testResultat2 = new Label("Trajet : " + testResultat);
+
+        this.getChildren().add(labelalgo);
+        this.getChildren().add(labelDistance);
+        this.getChildren().add(testResultat2);
 
     }
 }
