73230598491f8fa6c34d65ed324123a87c3e4769
AltantL <antoine.lapluye@gmail.com>
2025-06-04 05:27:03 +0200

    petit oublie

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,10 @@
-module com.example.sae_2012 {
+module vue {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires java.desktop;
 
-    opens com.example.sae_2012 to javafx.fxml;
-    exports com.example.sae_2012;
+    opens vue to javafx.fxml;
     exports vue;
 }
